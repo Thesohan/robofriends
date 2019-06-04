ecdd93e658db975de2b15d61a28e81d0e86a4237
sohan <sohan.cse16@nituk.ac.in>
2019-06-04 19:56:29 +0530

    project completed

---
+++ precache-manifest.75dfa4d6650f22e87d467448592d620e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75b28157326558e8d229301b7d3b87ab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e5899c35b3800f2f9611",
+    "url": "/static/css/2.e40df323.chunk.css"
+  },
+  {
+    "revision": "b81b1aa362d00a2fdf4c",
+    "url": "/static/css/main.9b7c64e0.chunk.css"
+  },
+  {
+    "revision": "e5899c35b3800f2f9611",
+    "url": "/static/js/2.b0f4c8b7.chunk.js"
+  },
+  {
+    "revision": "b81b1aa362d00a2fdf4c",
+    "url": "/static/js/main.3fdecc5a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
